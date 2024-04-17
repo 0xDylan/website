05fd5d44ddc86d6ae0383f855abcdac55dd3eef5
dylan <dylanszeliga@student.hcps.org>
2024-04-17 23:38:35 +0000

    feat(main): commit rest of website, now to deploy!

--- public/astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/serverless";
-
-// https://astro.build/config
-export default defineConfig({
-  output: "server",
-  adapter: vercel()
-});
---
+++ public/ts/index.ts
@@ -0,0 +1,1 @@
+// particles were going to go here, but I can't figure them out, so for now, they wont be here :(
