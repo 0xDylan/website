e4d7150510791a4e0c197769b8abc2ba3653dd0c
dylan <dylanszeliga@student.hcps.org>
2024-05-17 13:36:37 +0000

    fix(astro-config): fix errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,12 +15,13 @@ export default defineConfig({
         '/old/projects': {
             status: 302,
             destination: '/old_projects',
-        }
+        },
         'old/tools/': {
             status: 302,
             destination: '/old_tools',
-    }
-}
+        },
+    },
+    
     output: 'server',
     adapter: vercel({ webAnalytics: {enabled : true} })
 });
