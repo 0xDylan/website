c14a275eecd11ab7d93b24dfc6aee9b88e5ff982
dylan <shxrk@shxrk.dev>
2024-10-07 13:55:27 +0000

    chore(deps): update deps && move to pnpm

--- public/ts/index.ts
+++
@@ -1,5 +0,0 @@
-// particles were going to go here, but I can't figure them out, so for now, they wont be here :(
-
-console.log('hello traveller!');
-console.log('there isn\'t really anything to type here');
-console.log('but you\'re still here...');
